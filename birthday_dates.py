c2b25111b8bdc58aae83efcc1b4c6d3537b8676e
felixNyalenda <felixasejunior@gmail.com>
2024-01-30 12:19:47 +0300

    exercise 1 & 2

---
+++ birthday_dates.py
@@ -0,0 +1,22 @@
+# Create a program where you prompt a user for their age and return the year they were born.
+# After that, return the year they will turn 80.
+
+import datetime
+
+def calculate_birthyear(age):
+    current_year = datetime.datetime.now().year
+    birth_year = current_year - age
+    return birth_year
+
+def main():
+    try:
+        age = int(input("Enter your age: "))
+        birth_year = calculate_birthyear(age)
+        age_at_80 = birth_year + 80
+        print(f"Your birth year is {birth_year}, and you'll turn 80 in {age_at_80}.")
+    except:
+        print("Invalid input entered")
+
+
+main()
+
---
+++ tip_calculator.py
@@ -0,0 +1,27 @@
+# Create a program that lets the user input their total bill at a restaurant, and returns the total cost of the bill including
+# a tip. Use 15% of the total bill calculation.
+# Once you're finished, take this a step further and let the user enter their level of satisfaction with their service and factor
+# that value into the tip percentage. For example, if they received good service, the tip is 15%; great service is 20%; terrible service is 0%
+
+
+def bill_calculator(tip, bill):
+    tip_percent = tip / 100
+    total = bill + (bill * tip_percent)
+    return total
+
+def main():
+    try:
+        bill = int(input("Enter your bill amount: "))
+        satisfaction = input("What's your level of satisfaction?: ")
+        if satisfaction == "good service":
+            tip = 15
+        elif satisfaction == "great service":
+            tip = 20
+        else: tip = 0
+        total = bill_calculator(tip, bill)
+        print(f"Your total bill is Ksh {total}")
+    except:
+        print("Invalid input format")
+
+
+main()
